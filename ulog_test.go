5d303ad23c66f4b8de9b7629fd85716d96dc9360
Seth Hoenig <shoenig@duck.com>
2024-07-25 00:46:19 +0000

    ci: update workflows

--- ulog_test.go
+++ ulog_test.go
@@ -18,7 +18,7 @@ func Test_New(t *testing.T) {
 }
 
 func Test_SetLevel(t *testing.T) {
-	t.Run("error", func(t *testing.T) {
+	t.Run("error", func(_ *testing.T) {
 		log := New("test-error", SetLevel(Error))
 		log.E.Fmt("this is error")
 		log.W.Fmt("should not see warn")
@@ -27,7 +27,7 @@ func Test_SetLevel(t *testing.T) {
 		log.T.Fmt("should not see trace")
 	})
 
-	t.Run("warn", func(t *testing.T) {
+	t.Run("warn", func(_ *testing.T) {
 		log := New("test-warn", SetLevel(Warn))
 		log.E.Fmt("this is error")
 		log.W.Fmt("this is warn")
@@ -36,7 +36,7 @@ func Test_SetLevel(t *testing.T) {
 		log.T.Fmt("should not see trace")
 	})
 
-	t.Run("info", func(t *testing.T) {
+	t.Run("info", func(_ *testing.T) {
 		log := New("test-info", SetLevel(Info))
 		log.E.Fmt("this is error")
 		log.W.Fmt("this is warn")
@@ -45,7 +45,7 @@ func Test_SetLevel(t *testing.T) {
 		log.T.Fmt("should not see trace")
 	})
 
-	t.Run("debug", func(t *testing.T) {
+	t.Run("debug", func(_ *testing.T) {
 		log := New("test-debug", SetLevel(Debug))
 		log.E.Fmt("this is error")
 		log.W.Fmt("this is warn")
@@ -54,7 +54,7 @@ func Test_SetLevel(t *testing.T) {
 		log.T.Fmt("should not see trace")
 	})
 
-	t.Run("trace", func(t *testing.T) {
+	t.Run("trace", func(_ *testing.T) {
 		log := New("test-trace", SetLevel(Trace))
 		log.E.Fmt("this is error")
 		log.W.Fmt("this is warn")
